18a0833feed9a663e84693c82a37885c19f57c3e
Christian Panter <pantercp@outlook.com>
2023-05-18 12:11:08 +0100

    Added intersection observer for navbar

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-
-
+// Burger Menu
 
 const menuBtn = document.querySelector('.menu-btn');
 const navBar = document.querySelector('.nav-bar');
@@ -17,3 +16,27 @@ menuBtn.addEventListener('click', () => {
     }
 });
 
+// Intersection Animations
+
+const header = document.querySelector("#header");
+const sectionOne = document.querySelector(".hero-section");
+
+const sectionOneOptions = {
+  rootMargin: "-200px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function (
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scrolled");
+    } else {
+      header.classList.remove("nav-scrolled");
+    }
+  });
+},
+  sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);

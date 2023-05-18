e9fc2f31db5b347b4622bbfa8d1f2cd16321d2be
Christian Panter <pantercp@outlook.com>
2023-05-18 12:00:42 +0100

    Added styling to drop list for mobile

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 
-console.log("connected");
+
 
 const menuBtn = document.querySelector('.menu-btn');
+const navBar = document.querySelector('.nav-bar');
 let menuOpen = false;
 
 menuBtn.addEventListener('click', () => {
     if (!menuOpen) {
         menuBtn.classList.add('open');
+        navBar.classList.add('open');
         menuOpen = true;
     } else {
         menuBtn.classList.remove('open');
+        navBar.classList.remove('open');
         menuOpen = false;
     }
 });

a89f356897771f98a0fe68edd51cbd67945317f6
Christian Panter <pantercp@outlook.com>
2023-06-01 11:52:15 +0100

    Added testimonials carousel

--- app.js
+++ app.js
@@ -65,3 +65,61 @@ const appearOnScroll = new IntersectionObserver
 faders.forEach(fader => {
   appearOnScroll.observe(fader);
 })
+
+
+// Testimonial Carousel
+
+const testimonials = [
+  {
+    name: "Reece Cooper",
+    job: "Professional Photographer",
+    image: "images/chalk.jpg",
+    testimonial:
+      "What can I say except the best chalk I have ever used, made out of fully compostable packaging and a brilliant local company. If any of you guys are looking for a better alternative to big branded chalk then look no further than Peak Chalk.",
+  },
+  {
+    name: "Kris",
+    job: "Climbing Blogger",
+    image: "images/chalk.jpg",
+    testimonial:
+      "It lasts on your hands for a while - that is what I am looking for. If you love super fune chalk like me, this is the right chalk for you.",
+  },
+  {
+    name: "Jae Robin",
+    job: "UI Designer, Affinity Agency",
+    image: "images/chalk.jpg",
+    testimonial:
+      "Orci nulla pellentesque dignissim enim sit amet venenatis urna cursus eget nunc scelerisque viverra mauris in aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod in pellentesque massa placerat duis ultricies lacus sed turpis",
+  },
+  {
+    name: "Nicola Blakely",
+    job: "CEO,Zeal Wheels",
+    image: "images/chalk.jpg",
+    testimonial:
+      "Sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit",
+  },
+];
+//Current Slide
+let i = 0;
+//Total Slides
+let j = testimonials.length;
+let testimonialContainer = document.getElementById("testimonial-container");
+let nextBtn = document.getElementById("next");
+let prevBtn = document.getElementById("prev");
+nextBtn.addEventListener("click", () => {
+  i = (j + i + 1) % j;
+  displayTestimonial();
+});
+prevBtn.addEventListener("click", () => {
+  i = (j + i - 1) % j;
+  displayTestimonial();
+});
+let displayTestimonial = () => {
+  testimonialContainer.innerHTML = `
+    <p>${testimonials[i].testimonial}</p>
+    <img src=${testimonials[i].image}>
+    <h3>${testimonials[i].name}</h3>
+    <h6>${testimonials[i].job}</h6>
+  `;
+};
+window.onload = displayTestimonial;

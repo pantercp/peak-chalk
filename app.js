626edca7d839dd935a692b9e288a3be19f25df82
Christian Panter <pantercp@outlook.com>
2023-06-04 23:52:13 +0100

    minor tweak

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ const testimonials = [
     job: "CEO,Zeal Wheels",
     image: "images/chalk.jpg",
     testimonial:
-      "Sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit",
+      "Sit amet, consectetur adipiscing, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit",
   },
 ];
 //Current Slide
